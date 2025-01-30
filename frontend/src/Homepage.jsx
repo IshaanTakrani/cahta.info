0f5fee214ab90eceb964df084eef3dcb9f2583f8
IshaanTakrani <ishaantakrani@gmail.com>
2025-01-29 21:17:40 -0500

    update

---
+++ frontend/src/Homepage.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import React from "react";
+
+function Homepage() {
+  const [count, setCount] = useState(0);
+
+  return <div>Homepage</div>;
+}
+
+export default Homepage;

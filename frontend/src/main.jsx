9e8ff71a9193bc92506fb69f2a60767149f5f8c9
IshaanTakrani <ishaantakrani@gmail.com>
2025-02-08 00:28:04 -0500

    added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import Homepage from "./Homepage.jsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Counter from './Counter.jsx';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Homepage></Homepage>
-  </StrictMode>
+createRoot(document.getElementById('root')).render(
+	<StrictMode>
+		<Counter></Counter>
+	</StrictMode>
 );

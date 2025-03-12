ae6baae9b76b6e8148e796732f1dee1abe6063bb
IshaanTakrani <ishaantakrani@gmail.com>
2025-03-12 00:10:03 -0400

    added layout, image for homepage

--- frontend_react/src/Homepage.jsx
+++ frontend_react/src/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Typography from '@mui/joy/Typography';
 import Sheet from '@mui/joy/Sheet';
+import Box from '@mui/joy/Box';
 
 function Homepage() {
 	return (
@@ -13,6 +14,7 @@ function Homepage() {
 					alignItems: 'center',
 					m: 1,
 					p: 5,
+					py: 15,
 					borderRadius: 20,
 					bgcolor: 'background.body',
 					color: 'text.primary',
@@ -36,6 +38,39 @@ function Homepage() {
 				>
 					Catch Smarter. Tread Lighter. Sustain Our Oceans.
 				</Typography>
+				<Sheet
+					variant="outlined"
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						p: 2,
+						borderRadius: 5,
+						my: 5,
+						bgcolor: 'background.body',
+						color: 'text.primary',
+						minWidth: '40%',
+						maxWidth: '50%',
+					}}
+				>
+					<Box>
+						<Typography
+							sx={{
+								fontFamily: 'Lora',
+								fontSize: '1em',
+							}}
+						>
+							CAHTA introduces a groundbreaking bottom trawling net that uses
+							sound technology to attract groundfish while minimizing
+							environmental impact and reducing fuel costs.
+						</Typography>
+						<img
+							style={{ width: '100%', padding: '10px' }}
+							src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+							alt="Empty placeholder image, no content"
+						/>
+					</Box>
+				</Sheet>
 			</Sheet>
 		</div>
 	);
